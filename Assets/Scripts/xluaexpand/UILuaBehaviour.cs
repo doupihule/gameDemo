27b4c159b4e84a9980e0e68aa2d415623ab75604
xiangdu <xiangdu@playcrab.com>
2020-08-24 00:12:22 +0800

    提交uiluabehaviour

--- Assets/Scripts/xluaexpand/UILuaBehaviour.cs
+++ Assets/Scripts/xluaexpand/UILuaBehaviour.cs
@@ -25,18 +25,9 @@ namespace XLua
             if (name.Contains("(Clone)"))
                 gameObject.name = name.Replace("(Clone)", "");
 
-            luaTable = XLuaBridge.GetInstance().XLuaDoUILuaTable(name,"main");
             //绑定的ui类一定有会__cobject.指向这个对象的Object 这是通用规则. lua和c#绑定的对象 
             luaTable.Set("__cobject", this.gameObject);
-            //luaTable.Set("__index", XLuaBridge.luaEnv.Global);
-            // Need be invoked before luaAwake()
-            XLuaHelper.FillCompoents(luaTable, gameObject, "{0}");
             GetFunction();
-            
-            if ( luaAwake != null )
-            {
-                luaAwake( luaTable );
-            }
 
         }
 
