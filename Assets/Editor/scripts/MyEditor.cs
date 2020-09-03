1b0c29d5d7d92c015110e9ddaf5ec5bcc677939d
xiangdu <xiangdu@playcrab.com>
2020-09-03 17:02:32 +0800

    整理资源目录

---
+++ Assets/Editor/scripts/MyEditor.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using LitJson;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+public class MyEditor : Editor
+{
+    //将所有游戏场景导出为JSON格式
+    [MenuItem("GameObject/ExportJSON")]
+
+    static void ExportJSON()
+    {
+
+        Transform rootObjTrans;
+        string filepath = Application.dataPath + @"/../../../assets_dev/bin/levelCfg/levelCfg.json";
+        FileInfo t = new FileInfo(filepath);
+        if (!File.Exists(filepath))
+        {
+            File.Delete(filepath);
+        }
+        StreamWriter sw = t.CreateText();
+
+        StringBuilder sb = new StringBuilder();
+        JsonWriter writer = new JsonWriter(sb);
+
+        foreach (UnityEditor.EditorBuildSettingsScene S in UnityEditor.EditorBuildSettings.scenes)
+        {
+            if (S.enabled)
+            {
+                string name = S.path;
+                EditorSceneManager.OpenScene(name);
+                writer.WriteObjectStart();
+                writer.WritePropertyName("scenes");
+                writer.WriteArrayStart();
+                writer.WriteObjectStart();
+                writer.WritePropertyName("name");
+                writer.Write(name);
+                writer.WritePropertyName("level");
+                writer.WriteObjectStart();
+                foreach (GameObject rootObj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+                {
+                    if (rootObj.name.IndexOf("level_") != -1)
+                    {
+
+                        Debug.Log("=>" + rootObj.name + "开始写入");
+                        var elementNum = 0;
+                        var playerNum = 0;
+                        var targetNum = 0;
+
+                        writer.WritePropertyName(rootObj.name);
+                        // writer.WriteObjectStart();
+                        writer.WriteArrayStart();
+                        rootObjTrans = rootObj.transform;
+                        foreach (GameObject obj in UnityEngine.Object.FindObjectsOfType(typeof(GameObject)))
+                        {
+                            if (obj.transform.parent == rootObjTrans)
+                            {
+
+                                if (obj.GetComponent<Param>() == null)
+                                {
+                                    Debug.LogWarning("\t" + rootObj.name + "中" + obj.name + "未添加Param脚本");
+                                    continue;
+                                }
+
+                                writer.WriteObjectStart();
+                                writer.WritePropertyName("name");
+                                if (obj.name.IndexOf(' ') != -1)
+                                {
+                                    string[] sArray = obj.name.Split(' ');
+                                    writer.Write(sArray[0]);
+                                    // Debug.Log("\t" + sArray[0]);
+                                }
+                                else
+                                {
+                                    writer.Write(obj.name);
+                                    // Debug.Log("\t" + obj.name);
+                                }
+                                Debug.Log("\t" + obj.name);
+
+
+                                writer.WritePropertyName("type");
+                                Param.ElementGroup type = obj.GetComponent<Param>().m_ElementGroup;
+                                switch (type)
+                                {
+                                    case Param.ElementGroup.Player:
+                                        playerNum++;
+                                        break;
+                                    case Param.ElementGroup.Target:
+                                        targetNum++;
+                                        break;
+                                }
+                                writer.Write(type + "");
+
+                                writer.WritePropertyName("transform");
+
+                                writer.WriteArrayStart();
+                                writer.Write(Math.Round(-obj.transform.position.x, 2));
+                                writer.Write(Math.Round(obj.transform.position.y, 2));
+                                writer.Write(Math.Round(obj.transform.position.z, 2));
+                                writer.Write(Math.Round(obj.transform.rotation.eulerAngles.x, 2));
+                                writer.Write(Math.Round(-obj.transform.rotation.eulerAngles.y, 2));
+                                writer.Write(Math.Round(obj.transform.rotation.eulerAngles.z, 2));
+                                writer.Write(Math.Round(obj.transform.localScale.x, 2));
+                                writer.Write(Math.Round(obj.transform.localScale.y, 2));
+                                writer.Write(Math.Round(obj.transform.localScale.z, 2));
+                                writer.WriteArrayEnd();
+
+                                writer.WritePropertyName("param");
+                                SerializableTest param = obj.GetComponent<Param>().param;
+                                writer.WriteObjectStart();
+                                foreach (FieldInfo p in param.GetType().GetFields())
+                                {
+                                    writer.WritePropertyName(p.Name.Split('_')[1]);
+
+                                    switch (p.GetValue(param).GetType().Name)
+                                    {
+                                        case "Boolean":
+                                            writer.Write(p.GetValue(param).ToString().Substring(0, 1));
+                                            break;
+                                        case "Int32":
+                                            writer.Write(int.Parse(p.GetValue(param).ToString()));
+                                            break;
+                                        case "Vector3":
+                                            writer.WriteArrayStart();
+                                            Vector3 tmp = (Vector3)p.GetValue(param);
+                                            writer.Write(Math.Round(tmp.x, 2));
+                                            writer.Write(Math.Round(tmp.y, 2));
+                                            writer.Write(Math.Round(tmp.z, 2));
+                                            writer.WriteArrayEnd();
+                                            break;
+                                        default:
+                                            writer.Write(p.GetValue(param).ToString());
+                                            break;
+                                    }
+                                }
+                                writer.WriteObjectEnd();
+
+                                writer.WriteObjectEnd();
+
+                                elementNum++;
+                            }
+                        }
+
+                        writer.WriteArrayEnd();
+
+                        if (playerNum == 0)
+                            Debug.LogError("\t" + rootObj.name + "未设置主角");
+                        if (targetNum == 0)
+                            Debug.LogError("\t" + rootObj.name + "未设置目标");
+                        Debug.Log("=>" + rootObj.name + "共写入" + elementNum + "个元素");
+                        // writer.WriteObjectEnd();
+                    }
+                }
+                writer.WriteObjectEnd();
+                writer.WriteObjectEnd();
+                writer.WriteArrayEnd();
+                writer.WriteObjectEnd();
+
+                Debug.Log("写入完成");
+            }
+        }
+
+        sw.WriteLine(sb.ToString());
+        sw.Close();
+        sw.Dispose();
+        AssetDatabase.Refresh();
+    }
+}
---
+++ Assets/Editor/scripts/Param.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Param : MonoBehaviour {
+
+    public enum ElementGroup {
+        Player,
+        Element,
+        Target
+    }
+
+    [Tooltip ("Player 玩家\nElement 元素\nTarget 目标")]
+    public ElementGroup m_ElementGroup = ElementGroup.Element;
+
+    public enum ElementType {
+        Wall,
+        WoodBox,
+        IronBox,
+        TNTBomb,
+        Ball,
+        IronCube,
+        TNTTrigger,
+        IronStrip,
+        Hostage,
+        Portal,
+        Custom
+    }
+
+    [Tooltip ("Wall 墙\nWoodBox 木箱\nIronBox 铁箱\nTNTBomb TNT炸弹\nBall 球\nIronCube 铁块\nTNTTrigger TNT触发器\nIronStrip 铁长条\nHostage 人质\nPortal 传送门\nCustom 自定义")]
+    public ElementType m_ElementType = ElementType.Custom;
+    private ElementType recentType = ElementType.Custom;
+    public SerializableTest param = new SerializableTest ();
+    // Use this for initialization
+    void Start () {
+        Debug.Log ("test");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log ("test2");
+    }
+
+    void OnValidate () {
+        if (recentType != m_ElementType) {
+            switch (m_ElementType) {
+                case ElementType.Wall:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.WoodBox:
+                    param.m_break = true;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.IronBox:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 2;
+                    break;
+                case ElementType.TNTBomb:
+                    param.m_break = true;
+                    // param.m_explode = true;
+                    param.m_rebound = false;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.Ball:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 3;
+                    break;
+                case ElementType.IronCube:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 4;
+                    break;
+                case ElementType.TNTTrigger:
+                    param.m_break = false;
+                    // param.m_explode = true;
+                    param.m_rebound = false;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.IronStrip:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = true;
+                    param.m_weight = 5;
+                    break;
+                case ElementType.Hostage:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = false;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.Portal:
+                    param.m_break = false;
+                    // param.m_explode = false;
+                    param.m_rebound = false;
+                    param.m_weight = 0;
+                    break;
+                case ElementType.Custom:
+                    break;
+            }
+            recentType = m_ElementType;
+        }
+
+    }
+
+}
+
+[System.Serializable]
+public class SerializableTest {
+
+    public bool m_pierce = false;
+    [Tooltip ("破坏")]
+    public bool m_break = false;
+    public int m_autoMoveSpeed = 0;
+    public Vector3 m_autoMoveDis = new Vector3 (0, 0, 0);
+    [Tooltip ("爆炸")]
+    public bool m_explodeButton = false;
+    public bool m_explodeBox = false;
+    public int m_explodeId = 0;
+    public int m_explodeRange = 0;
+    [Tooltip ("反弹")]
+    public bool m_rebound = false;
+    [Tooltip ("重量（0表示无此参数")]
+    public int m_weight = 0;
+    public bool m_portalIn = false;
+    public bool m_portalOut = false;
+    public int m_portalId = 0;
+
+}
